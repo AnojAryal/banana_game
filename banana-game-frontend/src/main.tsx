d8d8684ed021336777aa8f5ed911085cb513beda
AnojAryal <anoj1810@gmail.com>
2024-10-29 19:42:20 +0545

    added query client to main.tsx

--- banana-game-frontend/src/main.tsx
+++ banana-game-frontend/src/main.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import theme from "./theme"
+import theme from "./theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <Router>
+        <App />
+      </Router>
     </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
